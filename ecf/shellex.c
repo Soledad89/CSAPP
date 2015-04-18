db12d8321e5597efb77e060896bf8a8a026146d4
soledad <wangboshine@126.com>
2015-04-18 08:54:13 +0800

    update some files

--- ecf/shellex.c
+++ ecf/shellex.c
@@ -12,14 +12,14 @@ int main()
     char cmdline[MAXLINE]; /* Command line */
 
     while (1) {
-	/* Read */
-	printf("> ");                   
-	Fgets(cmdline, MAXLINE, stdin); 
-	if (feof(stdin))
-	    exit(0);
+    	/* Read */
+    	printf("> ");                   
+    	Fgets(cmdline, MAXLINE, stdin); 
+    	if (feof(stdin))
+    	    exit(0);
 
-	/* Evaluate */
-	eval(cmdline);
+    	/* Evaluate */
+    	eval(cmdline);
     } 
 }
 /* $end shellmain */
@@ -36,13 +36,13 @@ void eval(char *cmdline)
     strcpy(buf, cmdline);
     bg = parseline(buf, argv); 
     if (argv[0] == NULL)  
-	return;   /* Ignore empty lines */
+    	return;   /* Ignore empty lines */
 
     if (!builtin_command(argv)) { 
-	if ((pid = Fork()) == 0) {   /* Child runs user job */
-	    if (execve(argv[0], argv, environ) < 0) {
-		printf("%s: Command not found.\n", argv[0]);
-		exit(0);
+        if ((pid = Fork()) == 0) {   /* Child runs user job */
+	        if (execve(argv[0], argv, environ) < 0) {
+	    	printf("%s: Command not found.\n", argv[0]);
+	    	exit(0);
 	    }
 	}
 
@@ -62,9 +62,9 @@ void eval(char *cmdline)
 int builtin_command(char **argv) 
 {
     if (!strcmp(argv[0], "quit")) /* quit command */
-	exit(0);  
+	    exit(0);  
     if (!strcmp(argv[0], "&"))    /* Ignore singleton & */
-	return 1;
+	    return 1;
     return 0;                     /* Not a builtin command */
 }
 /* $end eval */
